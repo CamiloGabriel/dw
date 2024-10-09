7b41d16e1abba80e60a9f6fac49cefd9603842c8
Camilo Gabriel <camilo.gabrielrm@gmail.com>
2024-10-09 13:56:17 -0300

    feat: corrigindo def buscar_todos_dados_commodities

--- src/extract_load.py
+++ src/extract_load.py
@@ -7,7 +7,8 @@ import os
 
 # import variaveis de ambiente
 load_dotenv()
-commodities = ['CL = F', 'GC = F', 'SI = F']
+
+commodities = ['CL=F', 'GC=F', 'SI=F']
 
 DB_HOST = os.getenv('DB_HOST_PROD')
 DB_PORT = os.getenv('DB_PORT_PROD')
@@ -22,7 +23,7 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 
 # pegar cotacao dos ativos
-def buscar_dados_commodities(simbolo, periodo = '5d', intervalo = '1d'):
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
     ticker = yf.Ticker(simbolo) # ao invés de usar um response, a biblioteca yf tem o método ticker que retornará a cotação desejada
     dados = ticker.history(period=periodo, interval= intervalo)[['Close']] # ira retornar um dataframe apenas com a coluna 'Close', se quiser pegar cotações de hora em hora, basta tirar o 'Close'.
     dados['simbolo'] = simbolo # criar uma coluna nova ['simbolo'] onde ele irá receber o prório simbolo do parametro da função, para retornar o ticker consultado
@@ -34,11 +35,11 @@ def buscar_todos_dados_commodities(commodities):
     for simbolo in commodities:
         dados = buscar_dados_commodities(simbolo)
         todos_dados.append(dados) #para cada dado retornado do dataset irá ser passado o simbolo 
-        return pd.concat (todos_dados) #retornar e concatenar os dados
+    return pd.concat (todos_dados) #retornar e concatenar os dados
 
 # salvar no banco de dados
 def salvar_no_postgres(df, schema='public'):
-    df.to_sql('commodities', engine, if_exists = 'replace', index = True, index_label = 'Date', schema = schema)
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label = 'Date', schema=schema)
 
 
 if __name__ == '__main__':
